ca6daaad02cf84068a799bca8d78f7eacd989125
GitM423 <matthias.p.hoeh@gmail.com>
2020-07-08 16:50:36 +0200

    v0.0.2_js_ID

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 let name = document.getElementById('name')
 let email = document.getElementById('email')
-let form = document.getElementById('form')
+let area = document.getElementById('area')
 
 function contact() {
     console.log("Contact name: " + name.value)
     console.log("Contact email: " + email.value)
-    console.log("Contact message: \n " + form.value)
+    console.log("Contact message: \n " + area.value)
     alert(
         "\n The name you provided: \n\n" + name.value +
         "\n\n\n The email you contacted us with: \n\n" + email.value +
-        "\n\n\n The message you sent us: \n\n" + form.value)
+        "\n\n\n The message you sent us: \n\n" + area.value)
 }
